06a606deb31bcf6b9b7fd3f1cff2b82e6337f435
Dan Snyder <snyderdan46@gmail.com>
2016-01-23 21:57:22 -0500

    Converted to splash potions and added aging

--- src/com/dsnyder/fountainofyouth/FountainOfYouth.java
+++ src/com/dsnyder/fountainofyouth/FountainOfYouth.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.Potion;
 
 public class FountainOfYouth extends JavaPlugin {
 	public static final String VERSION_KEY		= "FOYVersion";
-	public static final String CURRENT_VERSION	= "0.0.1-ALPHA";
+	public static final String CURRENT_VERSION	= "0.1.0-ALPHA";
 	
 	static JavaPlugin main;
 	
@@ -33,14 +35,20 @@ public class FountainOfYouth extends JavaPlugin {
     		Command command, String label, String[] args) {
 		
 		if (command.getName().equalsIgnoreCase("youth")) {
-			if (args.length != 0) return false;
 			
+			if (args.length != 0) return false;
 			if (!(sender instanceof Player)) return false;
 			
 			Player player = (Player) sender;
 			
-			if (player.getItemInHand().getType().isEdible()) {
+			if (player.getItemInHand().getType().equals(Material.POTION)) {
+				
 				ItemStack items = player.getItemInHand();
+				
+				if (!Potion.fromItemStack(items).isSplash()) {
+					sender.sendMessage(ChatColor.RED + "Item must be a splash potion!");
+				}
+				
 				ItemMeta im = items.getItemMeta();
 			    List<String> lore = new ArrayList<>();
 			    if (im.hasLore()) {
@@ -52,7 +60,38 @@ public class FountainOfYouth extends JavaPlugin {
 			    player.sendMessage("Successfully enchanted");
 			    items.setItemMeta(im);
 			} else {
-				player.sendMessage(ChatColor.RED + "Item is not edible!");
+				player.sendMessage(ChatColor.RED + "Item must be a splash potion!");
+			}
+			
+			return true;
+			
+		}  else if (command.getName().equalsIgnoreCase("aging")) {
+			
+			if (args.length != 0) return false;
+			if (!(sender instanceof Player)) return false;
+			
+			Player player = (Player) sender;
+			
+			if (player.getItemInHand().getType().equals(Material.POTION)) {
+				
+				ItemStack items = player.getItemInHand();
+				
+				if (!Potion.fromItemStack(items).isSplash()) {
+					sender.sendMessage(ChatColor.RED + "Item must be a splash potion!");
+				}
+				
+				ItemMeta im = items.getItemMeta();
+			    List<String> lore = new ArrayList<>();
+			    if (im.hasLore()) {
+			    	lore = im.getLore();
+			    }
+			    
+			    lore.add("Aging I");
+			    im.setLore(lore);
+			    player.sendMessage("Successfully enchanted");
+			    items.setItemMeta(im);
+			} else {
+				player.sendMessage(ChatColor.RED + "Item must be a splash potion!");
 			}
 			
 			return true;
--- src/com/dsnyder/fountainofyouth/YouthListener.java
+++ src/com/dsnyder/fountainofyouth/YouthListener.java
@@ -1,11 +1,11 @@
 package com.dsnyder.fountainofyouth;
 
 import org.bukkit.entity.Ageable;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -13,19 +13,30 @@ import org.bukkit.inventory.ItemStack;
  */
 public class YouthListener implements Listener {
 	
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled=true)
-	public void PlayerInteractEvent(PlayerInteractEntityEvent e) {
-		Player player = e.getPlayer();
-		ItemStack item = player.getItemInHand();
+	
+	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onItemSplash(PotionSplashEvent event) {
 		
-		if (!item.getType().isEdible()) return;
+		ItemStack item = event.getPotion().getItem();
+		
 		if (!item.getItemMeta().hasLore()) return;
-		if (!item.getItemMeta().getLore().contains("Youth I")) return;
-		if (!(e.getRightClicked() instanceof Ageable)) return;
-		
-		Ageable ent = (Ageable) e.getRightClicked();
-		
-		ent.setBaby();
-		ent.setAgeLock(true);
+		if (item.getItemMeta().getLore().contains("Youth I")) {
+			for (Entity e : event.getAffectedEntities()) {
+				if (!(e instanceof Ageable)) continue;
+				
+				Ageable ent = (Ageable) e;
+				
+				ent.setBaby();
+				ent.setAgeLock(true);
+			}
+		} else if (item.getItemMeta().getLore().contains("Aging I")) {
+			for (Entity e : event.getAffectedEntities()) {
+				if (!(e instanceof Ageable)) continue;
+				
+				Ageable ent = (Ageable) e;
+				
+				ent.setAgeLock(false);
+			}
+		}
 	}
 }
